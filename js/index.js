29c7dce6681cdfec6521ebd3342bf630a67bd1d6
Laura_1998 <laurasaldel24@gmail.com>
2023-09-09 15:07:01 -0500

    nuevos cambios

--- js/index.js
+++ js/index.js
@@ -1,75 +1,87 @@
 import { array_projects } from "./data.js";
 
 // elemento de interfaz, se va a ejecutar cuando cargue el elemento, la página html
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const ocupaciones = document.querySelector(".ocupaciones");
+  const figure_scroll = document.querySelector(".figure_scroll");
 
-    
-    const ocupaciones = document.querySelector(".ocupaciones");
-    
-    function slider() {
-        setTimeout(()=>{
-            
-            ocupaciones.style.color="red";
-            ocupaciones.textContent="Ingeniera";
-            
-        },0);
+  function slider() {
+    setTimeout(() => {
 
-        setTimeout(()=>{
-            ocupaciones.textContent="Programadora";
-            ocupaciones.style.color="blue";
-            
-        },4000);
+      ocupaciones.style.color = "red";
+      ocupaciones.textContent = "Ingeniera";
 
-        setTimeout(()=>{
-            ocupaciones.textContent="Diseñadora";
-            ocupaciones.style.color="purple";
-            
-        },8000);
-    }
-    // slider();
-    // setInterval(slider,12000);
+    }, 0);
 
-    const listItemA = [...document.querySelectorAll(".list__a")];
-    const listElementB = [...document.querySelectorAll(".element__before")];
-    const input_menu = document.querySelector(".input_menu");
+    setTimeout(() => {
+      ocupaciones.textContent = "Programadora";
+      ocupaciones.style.color = "blue";
 
-    listItemA.map((item)=>{
-        item.addEventListener("click",(e)=>{
-            listElementB.map(itemB=>{
-                itemB.classList.remove("item_active");
-            })
+    }, 4000);
 
-            listElementB[parseInt(item.dataset.id)].classList.add("item_active");
-            if(input_menu.checked){
-                input_menu.checked = false;
-            }
-        })
+    setTimeout(() => {
+      ocupaciones.textContent = "Diseñadora";
+      ocupaciones.style.color = "purple";
+
+    }, 8000);
+  }
+
+  // slider();
+  // setInterval(slider, 12000);
+
+  const listItemA = [...document.querySelectorAll(".list__a")];
+  const listElementB = [...document.querySelectorAll(".element__before")];
+  const input_menu = document.querySelector(".input_menu");
+
+  listItemA.map((item) => {
+    item.addEventListener("click", (e) => {
+      listElementB.map(itemB => {
+        itemB.classList.remove("item_active");
+      })
+
+      listElementB[parseInt(item.dataset.id)].classList.add("item_active");
+      if (input_menu.checked) {
+        input_menu.checked = false;
+      }
     })
+  })
 
-    const list_section = [...document.querySelectorAll(".secciones")];
-    let h = window.innerHeight/3*2;
+  const list_section = [...document.querySelectorAll(".secciones")];
+  let h = window.innerHeight / 3 * 2;
 
-    const item_active =()=> {
-        // if(listItemA[0].getBoundingClientRect().top < h){
-    
-        //     listElementB[0].classList.add("item_active");
-        // }
-        list_section.map((item)=>{
-            if(item.getBoundingClientRect().top < h){
-                listElementB.map(span=>{
-                    span.classList.remove("item_active");
-                })
-                listElementB[parseInt(item.dataset.id)].classList.add("item_active");
-            }
+  const item_active = () => {
+    // if(listItemA[0].getBoundingClientRect().top < h){
+
+    //     listElementB[0].classList.add("item_active");
+    // }
+    list_section.map((item) => {
+      if (item.getBoundingClientRect().top < h) {
+        listElementB.map(span => {
+          span.classList.remove("item_active");
         })
-    }
-
-    // Evento para activar el item de la seccion presente en la vista
-    window.addEventListener("scroll",(e)=>{
-        item_active();
+        listElementB[parseInt(item.dataset.id)].classList.add("item_active");
+      }
     })
+  }
 
-    // función para filtrar proyectos
+  // Evento para activar el item de la seccion presente en la vista
+  window.addEventListener("scroll", (e) => {
+    item_active();
+
+    let scroll_top = window.scrollY;
+    if(scroll_top > 500){
+      figure_scroll.classList.add("scroll_active");
+    }else{
+      figure_scroll.classList.remove("scroll_active");
+    }
+  })
+
+  figure_scroll.addEventListener("click",()=>{
+    window.scrollTo(0,0);
+  })
+
+  // función para filtrar proyectos
 
   const cards__container = document.querySelector(".cards__container");
 
@@ -102,7 +114,14 @@ document.addEventListener("DOMContentLoaded",()=>{
   for (let i = 0; i < btn__projects.length; i++) {
     const btn = btn__projects[i];
     btn.addEventListener("click", (e) => {
+
+      btn__projects.forEach((btn_project)=>{
+        btn_project.classList.remove("btn_active");
+      });
+      btn.classList.add("btn_active");
+
       const btn_filter = e.target.innerText.toLowerCase();
+
       if (btn_filter === "todos") {
         filter_projects(array_projects);
       } else {
@@ -111,6 +130,8 @@ document.addEventListener("DOMContentLoaded",()=>{
       }
     })
   }
+
+
 })
 
 
